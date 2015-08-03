bce5bbce5b9cbb1b193ac6d3a7153b8895d4e21a
taranamol <tarskaur@gmail.com>
2015-08-02 17:13:02 -0700

    with some css changes

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -17,7 +17,7 @@ class CreaturesController < ApplicationController
     creature_params = params.require(:creature).permit(:name, :description)
     @creature = Creature.new(creature_params)
     if @creature.save
-      redirect_to "/creatures/#{@creature.id}"
+      redirect_to "/creatures/"
     end
   end
 
